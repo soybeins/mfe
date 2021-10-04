754509ad29c2a145cdac5b0f481e1a5c247a7e29
soybeins <renzolebumfacil@gmail.com>
2021-10-04 21:24:05 +0800

    added header files

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
+import {BrowserRouter} from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
-    return ( <MarketingApp />  );
+    return ( 
+        <BrowserRouter>
+            <div>
+            
+                <Header />
+                <MarketingApp />  
+            
+            </div>
+        </BrowserRouter>
+        );
 }
