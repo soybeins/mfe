eaee6f515443c47217e723c7c8bd921b8c263d2b
soybeins <renzolebumfacil@gmail.com>
2021-10-04 15:38:00 +0800

    initial commit

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,14 @@
+import React, { useRef, useEffect} from "react";
+import { mount as marketingMount } from 'marketing/MarketingApp';
+
+export default () => {
+    const ref = useRef(null);
+
+    useEffect(()=> {
+        marketingMount(ref.current);
+    }, [])
+
+    return(
+        <div ref={ref} />
+    );
+};
